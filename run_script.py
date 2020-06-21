da9cae3e319108f8455ca24a59c3ea1787924f21
felki <felkamp@mail.ru>
2020-06-21 17:24:13 +0300

    created script for adding movies to database

---
+++ run_script.py
@@ -0,0 +1,24 @@
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "moviesforme.settings")
+django.setup()
+
+from scripts.add_movies import add_movies
+import argparse
+
+
+def main(object_name):
+    exec('add_' + args.add + '()')
+    print('Объекты добавлены')
+
+
+if __name__ == '__main__':
+    allowed_commands = ['movies', 'genres', 'users', 'keywords', 'ratings']
+    parser = argparse.ArgumentParser(description='Добавление объекта в базу данных')
+    parser.add_argument('-add', type=str, help='adding object in database')
+    args = parser.parse_args()
+    if args.add in allowed_commands:
+        main(args.add)
+    else:
+        print('Введите комманду из списка [movies, genres, users, keywords, ratings]')
---
+++ scripts/add_movies.py
@@ -0,0 +1,19 @@
+from movies.models import Movie
+import json
+
+
+def add_movies():
+    data = json.load(open('data/main_data.json', 'r', encoding='utf8'))
+    step = 1
+    for value in data[10:]:
+        print(step, '/', len(data))
+        m = Movie(title=value['title'],
+                  original_title=value['original_title'],
+                  tagline=value['tagline'],
+                  imdb_id=value['imdb_id'],
+                  release_date=value['release_date'],
+                  overview=value['overview'],
+                  poster_path=value['poster_path'],
+                  backdrop_path=value['backdrop_path'])
+        m.save()
+        step += 1
