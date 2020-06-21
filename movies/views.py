d91e991e276aa101268edc3f0fce7f4715b387b9
felki <felkamp@mail.ru>
2020-06-21 22:52:20 +0300

    added view for the movie page

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from .models import Movie
 
 
 # Create your views here.
-def movie_page(request):
-    pass
+def movie_page(request, slug):
+    movie = get_object_or_404(Movie, slug__iexact=slug)
+    user_rating = None
+    similar_movies = []
+    return render(request, 'movies/movie_page.html',
+                  context={'movie': movie, 'similar_movies': similar_movies, 'user_rating': user_rating})
