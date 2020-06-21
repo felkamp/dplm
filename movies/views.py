1a3eec995d9067c097539d5f2731eba44628624d
felki <felkamp@mail.ru>
2020-06-21 23:34:30 +0300

    added view for the genres page

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
-from .models import Movie
+from .models import Movie, Genre
 
 
 # Create your views here.
@@ -10,3 +10,8 @@ def movie_page(request, slug):
     similar_movies = []
     return render(request, 'movies/movie_page.html',
                   context={'movie': movie, 'similar_movies': similar_movies, 'user_rating': user_rating})
+
+
+def genres_page(request):
+    genres = Genre.objects.all().order_by("name")
+    return render(request, 'movies/genres_page.html', context={'genres': genres})
