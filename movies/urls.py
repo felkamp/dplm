6fed02ca807870a3e3da194c64615fc16c736ca6
felki <felkamp@mail.ru>
2020-06-22 00:03:04 +0300

    added url for the genre page

--- movies/urls.py
+++ movies/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('genres/', genres_page, name='genres_page_url'),
+    path('genres/<str:slug>', genre_page, name='genre_page_url'),
     path('<str:slug>/', movie_page, name='movie_page_url'),
 ]
