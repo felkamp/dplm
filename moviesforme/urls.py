2276b83c3777bba497f7a25142b963392255afec
felki <felkamp@mail.ru>
2020-06-21 21:38:23 +0300

    included movies.urls

--- moviesforme/urls.py
+++ moviesforme/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .views import *
+from django.urls import include
 
 urlpatterns = [
     path('', main_page, name='main_page_url'),
     path('admin/', admin.site.urls),
+    path('movies/', include('movies.urls')),
 ]
