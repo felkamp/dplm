60a8c26a583697c47c9df721de0e378e37ad283a
felki <felkamp@mail.ru>
2020-06-21 23:37:01 +0300

    added url for the genres page

--- movies/urls.py
+++ movies/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import movie_page
+from .views import *
+
 urlpatterns = [
-    path('<str:slug>/', movie_page, name="movie_page_url"),
+    path('genres/', genres_page, name='genres_page_url'),
+    path('<str:slug>/', movie_page, name='movie_page_url'),
 ]
