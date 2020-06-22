e6eef855e18da155212011bde23724b4ac643fa4
felki <felkamp@mail.ru>
2020-06-22 19:29:33 +0300

    changed database sqlite3->postgresql

--- moviesforme/settings.py
+++ moviesforme/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'moviesforme.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mfm',
+        'USER': 'felki',
+        'PASSWORD': '616',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
