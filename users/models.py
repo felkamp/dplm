a502582c5082db336bfabbd9083f7540ee6ffae5
felki <felkamp@mail.ru>
2020-06-22 14:38:34 +0300

    added __str__ to User class

--- users/models.py
+++ users/models.py
@@ -1,28 +1,4 @@
-from django.db import models
-from django.forms import ModelForm
-from django import forms
 from django.contrib.auth.models import User
-
-
-# Create your models here.
-class RegistrationForm(ModelForm):
-    password = forms.CharField(label="Введите пароль", widget=forms.PasswordInput())
-    password2 = forms.CharField(widget=forms.PasswordInput())
-
-    class Meta:
-        model = User
-        fields = ['username', 'password', 'password2']
-
-    def clean_password2(self):
-        cd = self.cleaned_data
-        if cd['password'] != cd['password2']:
-            raise forms.ValidationError('Пароли не совпадают.')
-        return cd['password2']
-
-
-class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput())
 
 
 def get_username(self):
