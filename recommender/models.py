4b6ac3c14c2487b98f5728cb5730d9541e7a1299
felki <felkamp@mail.ru>
2020-07-01 17:01:15 +0300

    changed svd, user_based fields

--- recommender/models.py
+++ recommender/models.py
@@ -6,8 +6,8 @@ from django.contrib.postgres.fields import ArrayField
 # Create your models here.
 class Recommendation(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    svd = ArrayField(models.CharField(max_length=10), blank=True)
-    user_based = ArrayField(models.CharField(max_length=10), blank=True)
+    svd = ArrayField(models.CharField(max_length=10), blank=True, null=True)
+    user_based = ArrayField(models.CharField(max_length=10), blank=True, null=True)
 
     def __str__(self):
         return 'user' + self.user.username
