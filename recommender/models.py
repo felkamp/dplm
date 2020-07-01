3587e6505416c14af9e1a1e7eaee5d1a06d86428
felki <felkamp@mail.ru>
2020-07-01 15:38:24 +0300

    created model Recommedation

--- recommender/models.py
+++ recommender/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
+
 
 # Create your models here.
+class Recommendation(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    svd = ArrayField(models.CharField(max_length=10), blank=True)
+    user_based = ArrayField(models.CharField(max_length=10), blank=True)
+
+    def __str__(self):
+        return 'user' + self.user.username
